e27a12cce0d93a0b1ece5c0d41ca5be7aebf44a3
Mario Gómez <ma.gomez@duocuc.cl>
2023-10-30 12:16:47 -0300

    Added some components, some changes in design

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export default function Home(): React.ReactElement {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white select-none">
