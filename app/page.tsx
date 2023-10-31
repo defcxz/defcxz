713fb8bf679082905c010d67aee32c59faf0d706
Mario Gómez <ma.gomez@duocuc.cl>
2023-10-31 00:08:30 -0300

    Added footer!

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
-export default function Home(): React.ReactElement {
+export default function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center justify-center select-none">
       <div className="container flex flex-col items-center justify-center px-4 py-16 ">
-        <p className="animate-pulse font-regular tracking-wide text-5xl text-orange-400" style={{textShadow: "#ff9b71 1px 0 20px"}}>
+        <p className="font-bold tracking-wide text-[5rem] text-orange-400" style={{textShadow: "#ff9b71 1px 0 20px"}}>
           マリオ
         </p>
       </div>
