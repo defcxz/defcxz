9d4a489343390490f8bf1139bdea1515efa7774e
Mario Gómez <ma.gomez@duocuc.cl>
2024-01-05 23:02:44 -0300

    Added some epic background pt.2

--- app/components/GradientBackground/page.tsx
+++ app/components/GradientBackground/page.tsx
@@ -59,7 +59,7 @@ const animation = {
 export default function GradientBackground() {
   return (
     <motion.span
-      className={'absolute w-full h-full z-[-1]'}
+      className={'absolute w-full h-full z-[-1] t-calc(50% + 1rem)'}
       variants={animation}
       initial="hiddenBg"
       animate="visibleBg"
@@ -74,7 +74,7 @@ export default function GradientBackground() {
       >
         <ShaderGradient
           control='query'
-          urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.3&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false'
+          urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%23ff5005&color2=%23dbba95&color3=%23000000&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0.3&positionY=-0.3&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=50&shader=positionMix&toggleAxis=false&type=plane&uAmplitude=1.4&uDensity=1.8&uFrequency=5.5&uSpeed=0.1&uStrength=3.8&uTime=0&wireframe=false&zoomOut=false'
         />
       </ShaderGradientCanvas>
     </motion.span>
