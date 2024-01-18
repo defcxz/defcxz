28242ad6ade2a7fd82d36abc48bc28bef8a001c3
Mario Gómez <ma.gomez@duocuc.cl>
2024-01-18 15:11:35 +0000

    Update photography page layout

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -9,7 +9,7 @@ export default function Photo() {
           <div>
             <Image
               alt="Picture"
-              className="absolute -z-10 inset-0 object-cover w-full h-full"
+              className="absolute -z-10 inset-0 sm:w-full md:w-auto md:h-[100vh] md:mx-auto md:aspect-auto"
               height={1920}
               src={photo.url_foto}
               style={{
@@ -18,10 +18,10 @@ export default function Photo() {
               }}
               width={1080}
               />
-            <div className="absolute inset-0 flex flex-col justify-end p-6">
-              <h1 className="text-lg font-bold text-white">{photo.titulo}</h1>
-              <p className="text-sm text-white mt-2">{photo.descripcion}</p>
-              <div className="flex items-center gap-2 mt-4 text-xs text-white">
+            <div className="absolute inset-0 flex flex-col justify-end p-6 sm:w-100 md:w-1/2">
+              <h1 className="text-lg font-bold text-white drop-shadow">{photo.titulo}</h1>
+              <p className="text-sm text-white mt-2 drop-shadow">{photo.descripcion}</p>
+              <div className="flex items-center gap-2 mt-4 text-xs text-white drop-shadow">
                 <LocateIcon className="w-3 h-3" />
                 <span>{photo.ubicacion}</span>
               </div>
