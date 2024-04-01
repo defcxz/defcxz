f5cda4a72e22c447ed1bf0d0d4e1e6ccaae14f93
Mario Gómez <ma.gomez@duocuc.cl>
2024-04-01 12:42:17 +0000

    Rollback

--- app/lib/notion.ts
+++ app/lib/notion.ts
@@ -1,8 +1,10 @@
+
 import { Client } from '@notionhq/client';
 import {
   BlockObjectResponse,
   PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+
 
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -10,59 +12,44 @@ export const notion = new Client({
 
 export const revalidate = 0;
 
-export async function getStaticProps() {
-  const pages = await fetchPages();
 
-  return {
-    props: {
-      pages,
-    },
-    revalidate,
-  };
-}
-
-export async function getStaticPaths() {
-  const pages = await fetchPages();
-  const paths = pages.map((page) => ({
-    params: { slug: (page.properties.Slug as { rich_text: { plain_text: string }[] }).rich_text[0].plain_text},
-  }));
-
-  return { paths, fallback: false };
-}
-
-export async function fetchPages() {
-  const res = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID!,
-    filter: {
-      property: "Status",
-      status: {
-        equals: "Publicado",
+export const fetchPages = (async () => {
+  const res = await notion.databases
+    .query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property: "Status",
+        status: {
+          equals: "Publicado",
+        }
       },
-    },
-    sorts: [
-      {
-        property: "Created",
-        direction: "ascending",
-      },
-    ],
-  });
+      sorts: [
+        {
+          property: "Created",
+          direction: "descending"
+        }
+      ]
+    });
   return res.results as PageObjectResponse[];
-}
+});
 
-export async function fetchPageBySlug(slug: string) {
-  const res = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID!,
-    filter: {
-      property: "Slug",
-      rich_text: {
-        equals: slug,
+
+export const fetchPageBySlug = (async (slug: string) => {
+  const res = await notion.databases
+    .query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property: "Slug",
+        rich_text: {
+          equals: slug,
+        },
       },
-    },
-  });
+    });
   return res.results[0] as PageObjectResponse;
-}
+});
 
-export async function fetchPageBlocks(pageId: string) {
-  const res = await notion.blocks.children.list({ block_id: pageId });
+export const fetchPageBlocks = async (pageId: string) => {
+  const res = await notion.blocks.children
+    .list({ block_id: pageId });
   return res.results as BlockObjectResponse[];
-}
+};
