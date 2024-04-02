5475a64ba3c3840b6a2d69fa3c891344bf8e68c3
Mario Gómez <ma.gomez@duocuc.cl>
2024-04-02 00:11:05 -0300

    OG Test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,26 +6,9 @@ import * as React from "react";
 import Link from "next/link";
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
-import { Metadata } from 'next';
 
-type Props = {
-  params: {
-    slug: string
-  }
-}
 
-export const runtime = 'edge';
 
-export const generateMetadata = async (
-  props: Props
-): Promise<Metadata> => {
-  const { params } = props
-  const post = await fetchPageBySlug(params.slug)
-  const title = (post?.properties.Title as { title: { plain_text: string }[] }).title[0].plain_text;
-  return {
-    title: title + ' - Mario G.',
-  };
-};
 export default async function Page({ params }: { params: { slug: string } }) {
   const post = await fetchPageBySlug(params.slug);
   if (!post) notFound();
@@ -44,18 +27,23 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const formattedDate = format(new Date(created_time), 'MMMM dd, yyyy', { locale: es });
 
   return (
-    <main className='mt-24 mx-auto w-11/12 md:max-w-2xl'>
-      <header>
-        <Link href={'/blog'} className='flex gap-3'>
-          {'<'}
-          <h1 className='text-xl font-bold'>Blog</h1>
-        </Link>
-      </header>
-      <div className='flex items-center mt-5 justify-between'>
-        <h2 className='text-lg font-bold'>{title}</h2>
-        <p className='opacity-65'>{formattedDate}</p>
-      </div>
-      <div className='notion-render mt-10' dangerouslySetInnerHTML={{__html: html}}></div>
-    </main>
+    <>
+      <head>
+        <title>{title}</title>
+      </head>
+      <main className='mt-24 mx-auto w-11/12 md:max-w-2xl'>
+        <header>
+          <Link href={'/blog'} className='flex gap-3'>
+            {'<'}
+            <h1 className='text-xl font-bold'>Blog</h1>
+          </Link>
+        </header>
+        <div className='flex items-center mt-5 justify-between'>
+          <h2 className='text-lg font-bold'>{title}</h2>
+          <p className='opacity-65'>{formattedDate}</p>
+        </div>
+        <div className='notion-render mt-10' dangerouslySetInnerHTML={{__html: html}}></div>
+      </main>
+    </>
   );
 }
