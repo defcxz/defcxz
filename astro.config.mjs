a1a89e7a9a6a6e0a9e274e9db1eb75bd6ba6252f
Mario G. <ma.gomez@duocuc.cl>
2025-09-01 22:40:51 -0400

    rollback and installed cloudflare integration with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -9,5 +9,7 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: cloudflare(),
 });
